c576480a0cfcfa12372a25719bfb1c6c0555ab4a
Oussama <oussama.thecreative@gmail.com>
2024-10-27 09:43:21 +0100

    edit

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-laa <oait-laa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 14:38:56 by oait-laa          #+#    #+#             */
-/*   Updated: 2024/10/18 10:14:03 by oait-laa         ###   ########.fr       */
+/*   Updated: 2024/10/27 09:42:43 by oait-laa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,16 @@
 
 int main()
 {
-	Zombie *z1 = zombieHorde(5, "da7mad");
-	z1->announce();
-	(z1 + 1)->announce();
-	(z1 + 2)->announce();
+	int i = 0;
+	int N = 5;
+	Zombie *z1 = zombieHorde(N, "juwi");
+	if (z1 == NULL)
+		return (1);
+	while (i < N)
+	{
+		z1[i].announce();
+		i++;
+	}
 	delete[] z1;
 	return (0);
 }
--- cpp01/ex01/zombieHorde.cpp
+++ cpp01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-laa <oait-laa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:51:42 by oait-laa          #+#    #+#             */
-/*   Updated: 2024/10/16 16:22:12 by oait-laa         ###   ########.fr       */
+/*   Updated: 2024/10/27 09:40:20 by oait-laa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 Zombie* zombieHorde( int N, std::string name )
 {
 	int i = 0;
+	if (N <= 0)
+	{
+		std::cerr << "N must be positive!\n";
+		return (NULL);
+	}
 	Zombie *zombies = new Zombie[N];
 	while (i < N)
 	{
