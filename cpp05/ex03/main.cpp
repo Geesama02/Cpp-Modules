20e5fa742e8d6ae3490e8b6442c941e7d1c57058
Geesama02 <oussama.thecreative@gmail.com>
2025-05-28 09:24:18 +0100

    fix 05

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-laa <oait-laa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 11:37:52 by oait-laa          #+#    #+#             */
-/*   Updated: 2025/05/27 11:50:52 by oait-laa         ###   ########.fr       */
+/*   Updated: 2025/05/28 09:23:31 by oait-laa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ int main() {
         AForm* rrf3;
         AForm* rrf4;
         rrf = someRandomIntern.makeForm("shrubbery creation", "Shrubbery");
-        rrf2 = someRandomIntern.makeForm("robotomy request", "Bender");
-        rrf3 = someRandomIntern.makeForm("presidental pardon", "President");
-        rrf4 = someRandomIntern.makeForm("random", "President");
         delete rrf;
+        rrf2 = someRandomIntern.makeForm("robotomy request", "Bender");
         delete rrf2;
+        rrf3 = someRandomIntern.makeForm("presidental pardon", "President");
         delete rrf3;
+        rrf4 = someRandomIntern.makeForm("random", "President");
         delete rrf4;
     }
     catch(std::exception& e)
